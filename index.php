c27f44d53290d09602fcb60d6f2e57d508265756
mahendra2044 <mahendra2044@gmail.com>
2020-04-23 21:39:14 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+include("header.php")
+?>
+  
+<div class="container">
+  <h3>KENDRIYA VIDYALAYA BHURKUNDA RESULT INFORMATION SYSTEM SESSION 2019-20</h3>
+    <p>This Portal is designed only for Quick Result dispaly for Class - 9, session 2019-20. </p>
+	<p>Use Result Menu to view the your result. It will ask your Admission No. to fetch the data from database. 
+	<p><p> <img src="image/result.jpg" class="img-responsive" alt="Image goes here"> </p></p>
+	
+	<h6><P> All efforts have been made to make the information as accurate as possible. Website will not be responsible for any loss or damage to any person caused by any short coming, defect or inaccuracy in the information available on this website.</p>
+	<p> Website designed and developed by Mahendra Rai, Computer Instructor, KV Bhurkunda. Contact at : mahendra2044@gmail.com </p></h6>
+               
+</div>
+
+</body>
+</html>
